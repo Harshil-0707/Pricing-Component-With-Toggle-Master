bd5e781e979831c0a98ef1e950fbbb92286c24d4
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-02-01 19:50:59 +0530

    initial commit

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const btn = document.getElementById("inner-button1");
 const amount = document.getElementsByClassName("amount");
 
 btn.onclick = () => {
-  btn.classList.toggle("active");
-  if (btn.classList.contains("active")) {
+  btn.classList.toggle("btn-toggle");
+  btn.style.transition = "all 0.4s";
+  if (btn.classList.contains("btn-toggle")) {
     amount[0].innerHTML = "$19.99";
     amount[1].innerHTML = "$24.99";
     amount[2].innerHTML = "$39.99";
